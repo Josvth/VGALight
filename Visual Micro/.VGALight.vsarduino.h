1093dd9d20aec0052bb82aa566dc976e2c00fdfe
Jos <josvanthof@hotmail.com>
2014-04-03 15:28:40 +0200

    Apparently I changed something.

--- Visual Micro/.VGALight.vsarduino.h
+++ Visual Micro/.VGALight.vsarduino.h
@@ -1,9 +1,22 @@
+/* 
+	Editor: http://www.visualmicro.com
+	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: Arduino Yún, Platform=avr, Package=arduino
+*/
+
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-//Board = Arduino Uno
-#define __AVR_ATmega328p__
-#define __AVR_ATmega328P__
-#define ARDUINO 150
+#define __AVR_ATmega32u4__
+#define __AVR_ATmega32U4__
+#define USB_VID 0x2341
+#define USB_PID 0x8041
+#define USB_MANUFACTURER 
+#define USB_PRODUCT "\"Arduino Yun\""
+#define ARDUINO 156
 #define ARDUINO_MAIN
 #define __AVR__
 #define __avr__
@@ -34,6 +47,6 @@ void setup(void);
 void loop(void);
 
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\cores\arduino\arduino.h"
-#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\variants\standard\pins_arduino.h" 
-#include "C:\Users\Luuk-Laptop\Source\Repos\VGALight\VGALight.ino"
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\variants\yun\pins_arduino.h" 
+#include "C:\Users\Jos\Source\Repos\VGALight\VGALight.ino"
 #endif
